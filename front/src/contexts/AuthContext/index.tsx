5b05a63c724e9b186ec53e23433ce9ad34f6d4ec
gauthierg <gauthier.ginier@aleia.com>
2025-01-21 21:37:48 -0400

    updated packages, add AuthContext context, and some of firebase logic in login component

--- front/src/contexts/AuthContext/index.tsx
+++ front/src/contexts/AuthContext/index.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { auth } from "@/firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = React.createContext<any>(null);
 
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [currentUser, setCurrentUser] = React.useState<any>(null);
-    const [loading, setLoading] = React.useState<boolean>(true);
-    const [userLoggedIn, setUserLoggedIn] = React.useState<boolean>(false);
+    const [currentUser, setCurrentUser] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, initializeUser);
+        return unsubscribe;
+    }, []);
 
+    async function initializeUser(user: any) {
+        if (user) {
+            setCurrentUser({ ...user });
+            setUserLoggedIn(true);
+        } else {
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading
+    };
+     
     return (
-        <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
-            {children}
+        <AuthContext.Provider value={value}>
+            {!loading && children}
         </AuthContext.Provider>
     );
 }
