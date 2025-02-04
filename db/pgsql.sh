020234d4e753ea6a0df67dfdc239d265193daf07
gauthierg <gauthier.ginier@aleia.com>
2025-02-03 22:39:50 -0400

    delete test of network arg in postgres container command

--- db/pgsql.sh
+++ db/pgsql.sh
@@ -1,2 +1,2 @@
 mkdir -p $PGSQL_PATH
-docker run --rm --network=host --name $PGSQL_CONTAINER_NAME -p $PGSQL_PORT:$PGSQL_PORT -e POSTGRES_USER=$STRAPI_USER -e POSTGRES_PASSWORD=$STRAPI_PASSWORD -e POSTGRES_DB=$DATABASE_NAME -v $PGSQL_PATH:/var/lib/postgresql/data postgres:latest
+docker run --rm --name $PGSQL_CONTAINER_NAME -p $PGSQL_PORT:$PGSQL_PORT -e POSTGRES_USER=$STRAPI_USER -e POSTGRES_PASSWORD=$STRAPI_PASSWORD -e POSTGRES_DB=$DATABASE_NAME -v $PGSQL_PATH:/var/lib/postgresql/data postgres:latest
